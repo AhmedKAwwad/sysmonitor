a277cd328039c860a7d91f9ecc7b2f1039078c45
AhmedKAwwad <a.k.awad97@gmail.com>
2021-10-06 16:32:05 -0400

    flask

--- hello.py
+++ hello.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from flask import request
 from markupsafe import escape
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
+    print ("Server is ON\n Welcome Awwad")
+    # shutdown_server()
     return "<p>Hello, World!</p>"
 
